3e4753cff484ad7ad1117c272f920e2f9c07d98a
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-19 00:40:39 +0500

    add express validation on create cart route

--- cartService/src/routes/cart.route.js
+++ cartService/src/routes/cart.route.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const { createCart } = require('../controllers/cart.controller');
-const { createAuthMiddleware } = require('../middlewares/auth.middleware');
+const express = require("express");
+const { createCart } = require("../controllers/cart.controller");
+const { createAuthMiddleware } = require("../middlewares/auth.middleware");
+const { createCartValidation } = require("../validator/cart.validator");
 
 const router = express.Router();
 
-
-router.post('/items', createAuthMiddleware(["user"]), createCart);
-
+router.post(
+  "/items",
+  createAuthMiddleware(["user"]),
+  createCartValidation,
+  createCart
+);
 
 module.exports = router;
---
+++ cartService/src/validator/cart.validator.js
@@ -0,0 +1,38 @@
+const { body, validationResult } = require("express-validator");
+const mongoose = require("mongoose");
+
+// Middleware to handle validation errors
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: "Validation failed",
+      errors: errors.array().map((error) => ({
+        field: error.param || error.path,
+        message: error.msg,
+        value: error.value,
+      })),
+    });
+  }
+  next();
+};
+
+// Validation rules for product creation
+const createCartValidation = [
+  body("productId")
+    .notEmpty()
+    .withMessage("Product ID is required")
+    .custom((value) => mongoose.Types.ObjectId.isValid(value))
+    .withMessage("Invalid product ID"),
+
+  body("quantity")
+    .notEmpty()
+    .withMessage("Quantity is required")
+    .isInt({ min: 1 })
+    .withMessage("Quantity must be at least 1"),
+  handleValidationErrors,
+];
+
+module.exports = {
+  createCartValidation,
+};
