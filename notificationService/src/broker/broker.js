0c3b131f3c0970f765c5dd674b350160dc2aea66
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-23 23:23:42 +0500

    notificationService broker setup

---
+++ notificationService/src/broker/broker.js
@@ -0,0 +1,53 @@
+const amqplib = require("amqplib");
+
+let channel, connection;
+
+async function connect() {
+  if (connection) return connection;
+
+  try {
+    connection = await amqplib.connect(process.env.RABITMQ_URL);
+    console.log("Connected to RabbitMq");
+    channel = await connection.createChannel();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function publishToQueue(queueName, data = {}) {
+    if (!channel || !connection) await connect();
+
+    await channel.assertQueue(queueName, {
+        durable: true
+    });
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
+    console.log('Message sent to queue:', queueName, data);
+}
+
+
+async function subscribeToQueue(queueName, callback) {
+
+    if (!channel || !connection) await connect();
+
+    await channel.assertQueue(queueName, {
+        durable: true
+    });
+
+    channel.consume(queueName, async (msg) => {
+        if (msg !== null) {
+            const data = JSON.parse(msg.content.toString());
+            await callback(data);
+            channel.ack(msg);
+        }
+    })
+
+}
+
+module.exports = {
+    connect,
+    channel,
+    connection,
+    publishToQueue,
+    subscribeToQueue
+}
